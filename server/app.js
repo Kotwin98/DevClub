ccb3bdf815983227895e04b308ab3c0a02df5215
Mateusz Kotwiński <mkotwinski98@gmail.com>
2022-08-12 08:48:52 +0200

    Add middleware for users

--- server/app.js
+++ server/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const usersRoutes = require('./routes/users-routes');
+
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+
+app.use('/api/users', usersRoutes);
 
 mongoose.connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true })
 .then(() => {
